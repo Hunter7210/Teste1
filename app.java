93302667f890f9c0f5fb4ceaf2931373dffa3895
Hunter7210 <114042717+Hunter7210@users.noreply.github.com>
2023-05-15 13:25:55 -0300

    Add files via upload

---
+++ app.java
@@ -0,0 +1,15 @@
+package Matrizes;
+
+import java.util.Scanner;
+
+/**
+ * app
+ */
+public class app {
+    public static void main(String[] args) {
+        MatrizesExemplo obj = new MatrizesExemplo();
+        obj.exemplo5();
+
+    }
+  
+}
